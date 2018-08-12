7b801256adcba4b386fd5cda3b7108fe8cad99df
Ledio <L.Bode515@gmail.com>
2018-08-12 19:45:19 -0400

    changed api response for auth

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,8 +3,8 @@ class User < ApplicationRecord
   has_secure_password
  
   # Basic password validation, configure to your liking.
-  validates_length_of       :password, maximum: 72, minimum: 8, allow_nil: true, allow_blank: false
-  validates_confirmation_of :password, allow_nil: true, allow_blank: false
+  validates_length_of       :password, maximum: 72, minimum: 3, allow_nil: false, allow_blank: false
+  validates_confirmation_of :password, allow_nil: false, allow_blank: false
   before_validation {
     (self.email = self.email.to_s.downcase) && (self.username = self.username.to_s.downcase)
   }
